57aff0314df3d901f7f79db0a51f600e7d389b77
Nitesh Turaga <nitesh.turaga@gmail.com>
2021-07-21 11:27:11 -0400

    Update manager_demo.R with sys variables for redis

--- inst/docker/manager_demo.R
+++ inst/docker/manager_demo.R
@@ -1,5 +1,8 @@
 library(RedisParam)
 
+Sys.setenv(REDIS_HOST = Sys.getenv("REDIS_SERVICE_HOST"))
+Sys.setenv(REDIS_PORT = Sys.getenv("REDIS_SERVICE_PORT"))
+
 p <- RedisParam(workers = 5, jobname = "binarybuild", is.worker = FALSE)
 
 fun <- function(i) {
